e1e85938aa6df3a78b6441be46c507fe67cffe53
Pretoi <33015911+Pretoi@users.noreply.github.com>
2017-11-01 13:24:37 +0100

    m

--- ordbank/testController.js
+++ ordbank/testController.js
@@ -4,104 +4,146 @@ var ordBank=new Array;
 var ordArray=new Array;
 var forsokt=new Array;
 var ord;
-
-var spill = document.getElementById("spill")
-
-
-var inpBokstav = document.getElementById("inpBokstav");
-var gjettKnapp = document.getElementById("gjettKnapp");
-
+var input;
+var liv = 5;
 var feilBokstavTekst = document.getElementById("feilBokstavTekst");
 var ordValgTekst = document.getElementById("ordValgTekst");
-var lengdeTekst = document.getElementById("lengdeTekst");
-var livTekst = document.getElementById("livTekst");
-var riktigBokstavTekst = document.getElementById("riktigBokstavTekst");
-//alfabetet i array
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
+var livigjen = document.getElementById("livTekst")
+
+
+
+
+
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
     ,'s','t','u','v','w','x','y','z','æ','ø','å'];
+
+
+
+
 
 $.getJSON("ordBank.json", function(data) {
   for(i=0; i<data.ordliste.length; i++) {
     ordBank [i]=new Array;
     ordBank [i][0]=data.ordliste [i].ord
   }
-  for (z in alphabet) {
-    document.getElementById("knapper").innerHTML += "<button id=" + z + ">" + alphabet[z] + "</button>";
-    }
-document.getElementById(z).onclick = function() {
-      knapper.style.visibility = 'hidden';
-    }
+  for(i=0;i<alphabet.length;i++){
+      $('#knapper').append('<button id="'+alphabet[i]+'" >'+alphabet[i]+'</button>');
+
+      }
+
+
+
+    /*  document.getElementById(x).onclick = function() {
+        knapper.style.visibility = 'hidden';
+
+      }*/
+
 
   neste();
 
   })
-//kaller pÃ¥ funksjon til Ã¥ finne ordet
+
+
+
+
+
+
+//kaller på funksjon til Ã¥ finne ordet
     function neste() {
       $('#spill').append('<div id="bokstav"></div>');
         finnord ();
         var n = ord.length;
-        var liv = n;
-        //disse er bare for testing purposes
         ordValgTekst.innerHTML += ord;
-        lengdeTekst.innerHTML += n;
-        livTekst.innerHTML += liv;
+        livigjen.innerHTML = liv
 
 
 
-//slager tiles for riktige bokstaver kalt t0,t1 osv
+//ska jo egentlig lag boksa, men vetdafaen
       var bokstaver=ord.length;
-        for(j=0;j<bokstaver;j++){
+              for(j=0;j<bokstaver;j++){
           $('#bokstav').append('<div class="tile" id=t'+j+'></div>');
 
 
+        //    alert(n);
     }
     }
 
-//velger ett ord fra ordbank array, samt deler det opp og setter d i en array
+
 
     function finnord() {
     var ordet=Math.floor(Math.random()*ordBank.length);
     ord=ordBank[ordet][0];
     ordArray=ord.split("");
-/* bare for å sjekke om koden er rett og den henter ut ett ord
+
+
+
+
+
+/*
     alert(ordet);
     alert(ord);
     alert(ordArray);
 */
   }
 
-  gjettKnapp.onclick = function(){
+
+//onclick funksjon for alfabetet
+  $(document.body).on('click', 'button', function() {
+    //disable knappen som blir trykket
+    $(this).attr('disabled', 'disabled');
+
 
     var gjettResultat = false;
-    var input = inpBokstav.value;
+    input = this.id;
+
+      //  var input = inpBokstav.value
 
     for(var x = 0;x<ordArray.length;x++){
       if(input == ordArray[x]){
-        gjettResultat = true;
-        //skriver til tile
         $('#t'+x).append(input);
-          }else{
-        gjettResultat = false;
-      }
-    }
-    if(gjettResultat == true){
-      //hvis bokstaven er i ordet, kjører funksjonen for å sjekke om hele ordet er riktig
-      sjekkSvar();
-
-    }else{
-      feilBokstavTekst.innerHTML += inpBokstav.value;
-    }
-  }
-//sjekker om svaret er riktig
-  function sjekkSvar() {
-    var svar = ""
-    for(i=0;i<ord.length;i++){
-      svar+=($('#t'+i).text());
-  }
-  if (svar==ord) {
-    alert("RIKTIG!!");
-
-  }
+        gjettResultat = true;
+          }
 }
+    if(gjettResultat){sjekkSvar();}
+    else{feil();}
+
+
+  })
+  //funksjon som sjekker om hele svaret er skrevet inn hvis en bokstav som er i ordet blir trykket.
+function sjekkSvar() {
+  var svar = "";
+  for (var i = 0; i < ord.length; i++) {
+    svar+=($('#t'+i).text());
+  }
+
+  if(svar==ord){
+    alert("DU VANT!")};
+  }
+//funksjon som skal redusere liv når en bokstav som ikke er i ordet blir trykket
+function feil() {
+  feilBokstavTekst.innerHTML += input;
+  liv-=1
+  livigjen.innerHTML = liv
+  if (liv<1) {
+    $("#spill").empty();
+    $('#spill').append('<div id="spillover">GAMEOVER!</div>');
+    $('#spill').append('<button id="reset" onClick="location.href=location.href">Restart</button>');
+}
+}
+/*
+function Restart() {
+  document.getElementById("reset").onclick = function(){
+    bokstaver.parentNode.removeChild(bokstav)
+
+}
+}
+/*function livcheck() {
+  liv-=1
+  if (liv<1) {
+    $('#spill').append('<div id="spillover">GAMEOVER!</div>');
+  }*/
+
+
+
 
 });
