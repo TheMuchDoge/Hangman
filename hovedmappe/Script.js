e28333d36403a5a398607b8a8bc12d490a472481
Pretoi <33015911+Pretoi@users.noreply.github.com>
2017-11-02 13:54:37 +0100

    i

--- hovedmappe/Script.js
+++ hovedmappe/Script.js
@@ -18,7 +18,7 @@ var valg = Number(Math.floor((Math.random() * ordAlt.length)));
 var ord = ordAlt[valg];
 
 ordArray=ord.split("");
-  
+
 for(i=0;i<alphabet.length;i++){
     $('#knapper').append('<button id="'+alphabet[i]+'" >'+alphabet[i]+'</button>');
   }
@@ -36,14 +36,14 @@ function neste() {
 
   //ska jo egentlig lag boksa, men vetdafaen
   var bokstaver=ord.length;
-  
+
   for(j=0;j<bokstaver;j++){
       $('#bokstav').append('<div class="tile" id=t'+j+'></div>');
     }
   }
 
 //onclick funksjon for alfabetet
-  $(document.body).on('click', 'button', function() {
+  $("#knapper").on('click', 'button', function() {
     //disable knappen som blir trykket
     $(this).attr('disabled', 'disabled');
 
@@ -79,11 +79,17 @@ function feil() {
   liv-=1
   livigjen.innerHTML = liv
   if (liv<1) {
-    $("#spill").empty();
+    $("#knapper").hide();
     $('#spill').append('<div id="spillover">GAMEOVER!</div>');
     $('#spill').append('<button id="reset" onClick="location.href=location.href">Restart</button>');
-  }
-}
+    for(var x = 0;x<ordArray.length;x++){
+      if(ordArray[x] == ordArray[x]){
+        if ($('#t'+x).is(':empty')){
+            $('#t'+x).append('<span id="feilbokover">'+ordArray[x]+'</span>');}
+          }
+        }
+      }
+    }
 
 
 function canvas(){
@@ -257,31 +263,4 @@ function kran() {
 
 function byttStil(stil){
   document.styleSheets[0].href = "ressurser/" + stil + ".css";
-  document.getElementById("lydSpor").src = "ressurser/" + stil + ".mp3"
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
