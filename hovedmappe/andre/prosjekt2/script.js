6fc6f3e882fb1983eb905ab6251de11aa4ec5c2a
Andreiyhh <andre.box3@hotmail.com>
2017-11-07 13:34:45 +0100

    Oppdatert

--- hovedmappe/andre/prosjekt2/script.js
+++ hovedmappe/andre/prosjekt2/script.js
@@ -294,9 +294,16 @@ function kran() {
 });
 
 function byttStil(stil){
-
   document.getElementById("cssLink").href = "styling_" + stil + ".css";
+  document.getElementById("overskrift").innerHTML = stil + " hangman";
+  document.getElementById("lydSpor").src = "Ressurser/" + stil + ".mp3";
+}
 
-  document.getElementById("lydSpor").src = "ressurser/" + stil + ".mp3";
-
+function muteLyd(){
+  var lyd = document.getElementById("lydSpor");
+  if(lydSpor.muted == false){
+    lydSpor.muted = true;
+  }else{
+    lydSpor.muted = false;
+  }
 }
