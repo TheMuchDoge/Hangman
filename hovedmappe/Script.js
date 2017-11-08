01d1f5bae6d495aa17bac72b1adf50f61fad4b85
blelias <33020817+blelias@users.noreply.github.com>
2017-11-08 13:21:25 +0100

    nnynny

--- hovedmappe/Script.js
+++ hovedmappe/Script.js
@@ -137,7 +137,7 @@ function sjekkSvar() {
   }
 
   if(svar==ord){
-        $('#spill').append('<div id="spillvant">DU VANT!</div>')};
+    alert("DU VANT!")};
   }
 
 //funksjon som skal redusere liv når en bokstav som ikke er i ordet blir trykket
@@ -162,101 +162,180 @@ function feil() {
 }
 }
 
-
-var canvasStil = location.hash.match(/#(\w+)/)[1];
-
-
-
 function canvas(){
   var canvas = document.querySelector('canvas');
   var c = canvas.getContext('2d');
 
   if (liv==7) {
-    var img2=new Image();
-    img2.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde2.gif";
-    img2.onload=function() {
-      c.clearRect(0,0,canvas.width,canvas.height);
-      c.drawImage(img2,0,0);
-    }
+    //Hode
+    c.beginPath();
+    c.lineWidth=2;
+    c.arc(300,200,25,0,2*Math.PI);
+    c.stroke();
+    //øyne
+    c.moveTo(290,190);
+    c.lineTo(290,195);
+    c.stroke();
+    c.moveTo(310,190);
+    c.lineTo(310,195);
+    c.stroke();
+    //munn
+    c.beginPath();
+    c.arc(300,205,5,0,Math.PI);
+    c.stroke();
   }
-  if (liv==6) {
-    var img3=new Image();
-    img3.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde3.gif";
-    img3.onload=function() {
-      c.clearRect(0,0,canvas.width,canvas.height);
-      c.drawImage(img3,0,0);
-    }
-  }
-  if (liv==5) {
-    var img4=new Image();
-    img4.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde4.gif";
-    img4.onload=function() {
-      c.clearRect(0,0,canvas.width,canvas.height);
-      c.drawImage(img4,0,0);
-    }
-  }
-  if (liv==4) {
-    var img5=new Image();
-    img5.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde5.gif";
-    img5.onload=function() {
-      c.clearRect(0,0,canvas.width,canvas.height);
-      c.drawImage(img5,0,0);
-    }
-  }
-  if (liv==3) {
-    var img6=new Image();
-    img6.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde6.gif";
-    img6.onload=function() {
-      c.clearRect(0,0,canvas.width,canvas.height);
-      c.drawImage(img6,0,0);
-    }
-  }
-  if (liv==2) {
-    var img7=new Image();
-    img7.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde7.gif";
-    img7.onload=function() {
-      c.clearRect(0,0,canvas.width,canvas.height);
-      c.drawImage(img7,0,0);
-    }
-  }
-  if (liv==1) {
-    var img8=new Image();
-    img8.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde8.gif";
-    img8.onload=function() {
-      c.clearRect(0,0,canvas.width,canvas.height);
-      c.drawImage(img8,0,0);
-    }
+
+if (liv==6) {
+  //Kropp
+  c.beginPath();
+  c.lineWidth=2;
+  c.moveTo(300, 225);
+  c.lineTo(300,325);
+  c.stroke();
+}
+
+if (liv==5) {
+  //høyre arm
+  c.moveTo(300,225);
+  c.lineWidth=2;
+  c.lineTo(350,275);
+  c.stroke();
+}
+
+if (liv==4) {
+  //Venstre arm
+  c.moveTo(300,225);
+  c.lineWidth=2;
+  c.lineTo(250,275);
+  c.stroke();
+}
+
+if (liv==3) {
+  //Høyre ben
+  c.moveTo(300,325);
+  c.lineWidth=2;
+  c.lineTo(330,395);
+  c.stroke();
+}
+
+if (liv==2) {
+  //Venstre ben
+  c.moveTo(300,325);
+  c.lineWidth=2;
+  c.lineTo(270,395);
+  c.stroke();
+}
+
+if (liv==1) {
+  c.clearRect(0,0,canvas.width,canvas.height);
+  //død mann
+  c.beginPath();
+  c.lineWidth=2;
+  c.arc(290,200,25,0,2*Math.PI);
+  c.stroke();
+  //kropp
+  c.beginPath();
+  c.lineWidth=2;
+  c.moveTo(300, 225);
+  c.lineTo(300,325);
+  c.stroke();
+  //høyre arm
+  c.moveTo(300,225);
+  c.lineWidth=2;
+  c.lineTo(310,275);
+  c.stroke();
+  //venste arm
+  c.moveTo(300,225);
+  c.lineWidth=2;
+  c.lineTo(290,275);
+  c.stroke();
+  //høyre ben
+  c.moveTo(300,325);
+  c.lineWidth=2;
+  c.lineTo(305,395);
+  c.stroke();
+  //venstre ben
+  c.moveTo(300,325);
+  c.lineWidth=2;
+  c.lineTo(295,395);
+  c.stroke();
+  //øyne
+  c.moveTo(300,190);
+  c.lineTo(295,195);
+  c.stroke();
+  c.moveTo(295,190);
+  c.lineTo(300,195);
+  c.stroke();
+  c.moveTo(280,195);
+  c.lineTo(285,200);
+  c.stroke();
+  c.moveTo(285,195);
+  c.lineTo(280,200);
+  c.stroke();
+  //munn
+  c.moveTo(290,215);
+  c.lineTo(300,210);
+  c.stroke();
+
+  //kran
+  c.beginPath();
+  c.lineWidth=13;
+  c.moveTo(50,500);
+  c.lineTo(100,450);
+  c.lineTo(100,100);
+  c.lineTo(305,100);
+  c.stroke()
+  c.lineWidth=4;
+  c.moveTo(300,100);
+  c.lineTo(300,175);
+  c.stroke();
+  c.beginPath();
+  c.lineWidth=13;
+  c.moveTo(150,500);
+  c.lineTo(100,450);
+  c.stroke();
+  c.beginPath();
+  c.lineWidth=7;
+  c.moveTo(100,150);
+  c.lineTo(150,100);
+  c.stroke();
   }
 }
 
 function kran() {
   var canvas = document.querySelector('canvas');
   var c = canvas.getContext('2d');
-  var img1=new Image();
-  img1.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde1.gif";
-  img1.onload=function(){
-    c.clearRect(0,0,canvas.width,canvas.height);
-    c.drawImage(img1,0,0);
-  }
-}
 
   //Tegning av krana
-  var canvas = document.querySelector('canvas');
-  var c = canvas.getContext('2d');
-  var img1=new Image();
-  img1.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde1.gif";
-  img1.onload=function(){
-    c.clearRect(0,0,canvas.width,canvas.height);
-    c.drawImage(img1,0,0);
-
-}
-}
+  c.clearRect(0,0,canvas.width,canvas.height);
+  c.beginPath();
+  c.lineWidth=13;
+  c.moveTo(50,500);
+  c.lineTo(100,450);
+  c.lineTo(100,100);
+  c.lineTo(305,100);
+  c.stroke()
+  c.lineWidth=4;
+  c.moveTo(300,100);
+  c.lineTo(300,175);
+  c.stroke();
+  c.beginPath();
+  c.lineWidth=13;
+  c.moveTo(150,500);
+  c.lineTo(100,450);
+  c.stroke();
+  c.beginPath();
+  c.lineWidth=7;
+  c.moveTo(100,150);
+  c.lineTo(150,100);
+  c.stroke();
+  }
+};
 
   //***************** HER JOBBER ELIAS
 
-  var hash = location.hash;
-  var startStil = location.hash.match(/#(\w+)/)[1];
-
+var hash = location.hash;
+var startStil = location.hash.match(/#(\w+)/)[1];
 
 function byttStil(stil){
   document.getElementById("cssLink").href = "styling_" + stil + ".css";
