0e531eb72935d227446e52799d742c8d25956fa1
Pretoi <33015911+Pretoi@users.noreply.github.com>
2017-11-09 16:01:42 +0100

    tv

--- Workaround/temp/Script.js
+++ Workaround/temp/Script.js
@@ -1,7 +1,6 @@
 $(function(){
     onPageLoad();
 });
-var input;
 function onPageLoad(){
 
 
@@ -11,7 +10,7 @@ var ordBank=new Array;
 var ordArray=new Array;
 var forsokt=new Array;
 //var ord;
-
+var input;
 var c;
 var canvas;
 let liv;
@@ -28,7 +27,6 @@ var ord = ordAlt[valg].toUpperCase();
 
 
 ordArray=ord.split("");
-
 for(i=0;i<alphabet.length;i++){
     $('#knapper').append('<button id="'+alphabet[i].toUpperCase()+'" >'+alphabet[i].toUpperCase()+'</button>');
   }
@@ -39,44 +37,51 @@ neste();
 function neste() {
   $('#spill').append('<div id="bokstav"></div>');
   //  finnord ();
-
   liv=7;
 $('#spill').append('<div id="livTekst">Du har '+liv+' liv igjen, ingen feil så langt!</div>');
-  $(document).on("keypress", tastatur);
-  // feilBokstavTekst.innerHTML = input & input2
-  kran();
 
-  //$('#spill').append('<button id="reset" onClick="location.href=location.href">Restart</button>');
-('#spill').append('<button id="reset">Restart</button>');
-$('#reset').on("click",function (){
+  $(document).on("keyup", tastatur);
+
+
+  $('#spill').append('<button id="reset" class="res">Restart</button>');
+  $('#reset').on("click",function (){
   while (spill.hasChildNodes()) {
-  spill.removeChild(spill.lastChild);
-}
-  while (knapper.hasChildNodes()) {
-  knapper.removeChild(knapper.lastChild);
-}
-    ordArray.length = 0
-    for(i=0;i<alphabet.length;i++){
-          forsokt.push(alphabet[i].toUpperCase());}
-          console.log("kokoko")
-  onPageLoad()
+    spill.removeChild(spill.lastChild);
+  }
+    while (knapper.hasChildNodes()) {
+    knapper.removeChild(knapper.lastChild);
+  }
+      ordArray.length = 0
+      for(i=0;i<alphabet.length;i++){
+            forsokt.push(alphabet[i].toUpperCase());}
+            console.log("jojojo")
+    onPageLoad()
 
-})
+  })
+  $('.resn').on("click",function(){
+    console.log(canvasStil)
+    kran();
+    canvasTegn();
+
+
+
+  })
+
   //ska jo egentlig lag boksa, men vetdafaen
+
   var bokstaver=ord.length;
 
   for(j=0;j<bokstaver;j++){
       $('#bokstav').append('<div class="tile" id=t'+j+'></div>');
     }
-
+kran();
   }
 
 
 
-  function tastatur(event) {
 
-    var knapp=event.keyCode;
-    if((knapp>64 && knapp<123) || knapp==230 || knapp == 198 || knapp == 248 || knapp==216 || knapp==229 || knapp==197){
+  function tastatur(event) {
+    if(event.keyCode>64 && event.keyCode<91){
           input = String.fromCharCode (event.keyCode).toUpperCase();
           forsoktboks = false
           riktig=false
@@ -144,211 +149,130 @@ function sjekkSvar() {
   }
 
   if(svar==ord){
-    skruAvInput();
-
-    c.clearRect(0,0,canvas.width,canvas.height);
-  };
+        $('#spill').append('<div id="spillvant">DU VANT!</div>');
+        skruAvInput();
+        c.clearRect(0,0,canvas.width,canvas.height);}
   }
 
 //funksjon som skal redusere liv når en bokstav som ikke er i ordet blir trykket
 function feil() {
-  canvasTegn();
 
   liv-=1
+  canvasTegn();
   livTekst.innerHTML = "Du har bare " + liv + " liv igjen!";
   if (liv<1) {
     skruAvInput();
+    livTekst.innerHTML = "Du har " + liv + " liv igjen!";
     $('#spill').append('<div id="spillover">GAMEOVER!</div>');
+    $(document).off("keypress", tastatur);
+    while (knapper.hasChildNodes()) {
+    knapper.removeChild(knapper.lastChild);}
     for(var x = 0;x<ordArray.length;x++){
       if(ordArray[x] == ordArray[x]){
         if ($('#t'+x).is(':empty')){
-            $('#t'+x).append('<span id="feilbokover">'+ordArray[x].toUpperCase()+'</span>');}
-            }
-          }
+            $('#t'+x).append('<span id="feilbokover">'+ordArray[x].toUpperCase()+'</span>');
+        }
 
+  }
+}
 }
 }
 function skruAvInput() {
   while (knapper.hasChildNodes()) {
   knapper.removeChild(knapper.lastChild);
   }
-  $(document).off("keypress", tastatur);
+  $(document).off("keyup", tastatur);
 }
 
-
+var canvasStil = location.hash.match(/#(\w+)/)[1];
 
 function canvasTegn(){
+  canvasStil = location.hash.match(/#(\w+)/)[1];
   canvas = document.querySelector('canvas');
   c = canvas.getContext('2d');
 
-  if (liv==7) {
-    //Hode
-    c.beginPath();
-    c.lineWidth=2;
-    c.arc(300,200,25,0,2*Math.PI);
-    c.stroke();
-    //øyne
-    c.moveTo(290,190);
-    c.lineTo(290,195);
-    c.stroke();
-    c.moveTo(310,190);
-    c.lineTo(310,195);
-    c.stroke();
-    //munn
-    c.beginPath();
-    c.arc(300,205,5,0,Math.PI);
-    c.stroke();
+  if (liv==6) {
+    var img2=new Image();
+    img2.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde2.gif";
+    img2.onload=function() {
+      c.clearRect(0,0,canvas.width,canvas.height);
+      c.drawImage(img2,0,0);
+    }
   }
-
-if (liv==6) {
-  //Kropp
-  c.beginPath();
-  c.lineWidth=2;
-  c.moveTo(300, 225);
-  c.lineTo(300,325);
-  c.stroke();
-}
-
-if (liv==5) {
-  //høyre arm
-  c.moveTo(300,225);
-  c.lineWidth=2;
-  c.lineTo(350,275);
-  c.stroke();
-}
-
-if (liv==4) {
-  //Venstre arm
-  c.moveTo(300,225);
-  c.lineWidth=2;
-  c.lineTo(250,275);
-  c.stroke();
-}
-
-if (liv==3) {
-  //Høyre ben
-  c.moveTo(300,325);
-  c.lineWidth=2;
-  c.lineTo(330,395);
-  c.stroke();
-}
-
-if (liv==2) {
-  //Venstre ben
-  c.moveTo(300,325);
-  c.lineWidth=2;
-  c.lineTo(270,395);
-  c.stroke();
-}
-
-if (liv==1) {
-  c.clearRect(0,0,canvas.width,canvas.height);
-  //død mann
-  c.beginPath();
-  c.lineWidth=2;
-  c.arc(290,200,25,0,2*Math.PI);
-  c.stroke();
-  //kropp
-  c.beginPath();
-  c.lineWidth=2;
-  c.moveTo(300, 225);
-  c.lineTo(300,325);
-  c.stroke();
-  //høyre arm
-  c.moveTo(300,225);
-  c.lineWidth=2;
-  c.lineTo(310,275);
-  c.stroke();
-  //venste arm
-  c.moveTo(300,225);
-  c.lineWidth=2;
-  c.lineTo(290,275);
-  c.stroke();
-  //høyre ben
-  c.moveTo(300,325);
-  c.lineWidth=2;
-  c.lineTo(305,395);
-  c.stroke();
-  //venstre ben
-  c.moveTo(300,325);
-  c.lineWidth=2;
-  c.lineTo(295,395);
-  c.stroke();
-  //øyne
-  c.moveTo(300,190);
-  c.lineTo(295,195);
-  c.stroke();
-  c.moveTo(295,190);
-  c.lineTo(300,195);
-  c.stroke();
-  c.moveTo(280,195);
-  c.lineTo(285,200);
-  c.stroke();
-  c.moveTo(285,195);
-  c.lineTo(280,200);
-  c.stroke();
-  //munn
-  c.moveTo(290,215);
-  c.lineTo(300,210);
-  c.stroke();
-
-  //kran
-  c.beginPath();
-  c.lineWidth=13;
-  c.moveTo(50,500);
-  c.lineTo(100,450);
-  c.lineTo(100,100);
-  c.lineTo(305,100);
-  c.stroke()
-  c.lineWidth=4;
-  c.moveTo(300,100);
-  c.lineTo(300,175);
-  c.stroke();
-  c.beginPath();
-  c.lineWidth=13;
-  c.moveTo(150,500);
-  c.lineTo(100,450);
-  c.stroke();
-  c.beginPath();
-  c.lineWidth=7;
-  c.moveTo(100,150);
-  c.lineTo(150,100);
-  c.stroke();
+  if (liv==5) {
+    var img3=new Image();
+    img3.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde3.gif";
+    img3.onload=function() {
+      c.clearRect(0,0,canvas.width,canvas.height);
+      c.drawImage(img3,0,0);
+    }
+  }
+  if (liv==4) {
+    var img4=new Image();
+    img4.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde4.gif";
+    img4.onload=function() {
+      c.clearRect(0,0,canvas.width,canvas.height);
+      c.drawImage(img4,0,0);
+    }
+  }
+  if (liv==3) {
+    var img5=new Image();
+    img5.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde5.gif";
+    img5.onload=function() {
+      c.clearRect(0,0,canvas.width,canvas.height);
+      c.drawImage(img5,0,0);
+    }
+  }
+  if (liv==2) {
+    var img6=new Image();
+    img6.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde6.gif";
+    img6.onload=function() {
+      c.clearRect(0,0,canvas.width,canvas.height);
+      c.drawImage(img6,0,0);
+    }
+  }
+  if (liv==1) {
+    var img7=new Image();
+    img7.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde7.gif";
+    img7.onload=function() {
+      c.clearRect(0,0,canvas.width,canvas.height);
+      c.drawImage(img7,0,0);
+    }
+  }
+  if (liv==0) {
+    var img8=new Image();
+    img8.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde8.gif";
+    img8.onload=function() {
+      c.clearRect(0,0,canvas.width,canvas.height);
+      c.drawImage(img8,0,0);
+    }
   }
 }
 
 function kran() {
   canvas = document.querySelector('canvas');
   c = canvas.getContext('2d');
+  canvasStil = location.hash.match(/#(\w+)/)[1];
+  var img1=new Image();
+  img1.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde1.gif";
+  img1.onload=function(){
+    c.clearRect(0,0,canvas.width,canvas.height);
+    c.drawImage(img1,0,0);
+  }
+}
 
   //Tegning av krana
-  c.clearRect(0,0,canvas.width,canvas.height);
-  c.beginPath();
-  c.lineWidth=13;
-  c.moveTo(50,500);
-  c.lineTo(100,450);
-  c.lineTo(100,100);
-  c.lineTo(305,100);
-  c.stroke()
-  c.lineWidth=4;
-  c.moveTo(300,100);
-  c.lineTo(300,175);
-  c.stroke();
-  c.beginPath();
-  c.lineWidth=13;
-  c.moveTo(150,500);
-  c.lineTo(100,450);
-  c.stroke();
-  c.beginPath();
-  c.lineWidth=7;
-  c.moveTo(100,150);
-  c.lineTo(150,100);
-  c.stroke();
-  }
-};
+/*  var img1=new Image();
+  img1.src="ressurser/" + canvasStil + "_canvas/" + canvasStil + "_bilde1.gif";
+  img1.onload=function(){
+    c.clearRect(0,0,canvas.width,canvas.height);
+    c.drawImage(img1,0,0);
+
+}*/
+}
 
   //***************** HER JOBBER ELIAS
-/*
+
 var hash = location.hash;
 var startStil = location.hash.match(/#(\w+)/)[1];
 
@@ -357,14 +281,16 @@ function byttStil(stil){
   document.getElementById("overskrift").innerHTML = stil + " hangman";
   document.getElementById("lydSpor").src = "ressurser/" + stil + ".mp3";
   document.getElementById("tittel").innerHTML = stil.toUpperCase() + " HANGMAN";
+  location.hash = stil;
 }
 
 function muteLyd(){
   var lyd = document.getElementById("lydSpor");
   if(lydSpor.muted == false){
     lydSpor.muted = true;
+    document.getElementById("muteknapp").innerHTML = "UNMUTE";
   }else{
     lydSpor.muted = false;
+    document.getElementById("muteknapp").innerHTML = "MUTE";
   }
 }
-*/
