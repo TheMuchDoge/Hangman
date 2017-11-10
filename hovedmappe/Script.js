79956a2dc1af6ef0408e3d6f1bcb5d9a0b930b55
blelias <33020817+blelias@users.noreply.github.com>
2017-11-10 13:41:07 +0100

    ord

--- hovedmappe/Script.js
+++ hovedmappe/Script.js
@@ -21,7 +21,7 @@ let liv;
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
 ,'s','t','u','v','w','x','y','z','æ','ø','å'];
 
-var ordAlt = ["damer", "spill", "datamaskin"];
+var ordAlt = ["damer", "spill", "datamaskin", "horer", "allah"];
 var valg;
 var ord;
 
